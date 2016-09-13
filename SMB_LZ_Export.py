65a5ada30a71cd1de5d15922be0677948bb0f5a6
Bobjrsenior <bobjrsenior@gmail.com>
2016-09-13 17:52:55 -0400

    Added method to write the file header.

--- SMB_LZ_Export.py
+++ SMB_LZ_Export.py
@@ -8,6 +8,8 @@ import os
 import bpy
 import mathutils
 import bpy_extras.io_utils
+import struct
+from array import array
 
 from bpy.props import (
         BoolProperty,
@@ -26,7 +28,7 @@ class SMBLZExporter(bpy.types.Operator):
     filepath = bpy.props.StringProperty(subtype='FILE_PATH')
     
     numCollisionFields = 0;
-    offsetToCollisionFields = 0
+    collisionFieldsOffset = 0
     sizeOfHeader = 160
     falloutPlaneOffset = 0
     falloutPlaneY = 0
@@ -67,14 +69,49 @@ class SMBLZExporter(bpy.types.Operator):
         return {'RUNNING_MODAL'}
     
     def writeZeroBytes(self, file, numZeros):
-        zeroChar = chr(0)
+        byteArray = []
         for i in range(0, numZeros):
-            file.write(zeroChar)
+            byteArray.append(0)
+        print(bytes(byteArray))
+        file.write(bytes(byteArray))
+            
+    def writeHeader(self, file):
+        self.writeZeroBytes(file, 8)
+        file.write(self.toBigI(self.numCollisionFields))
+        file.write(self.toBigI(self.collisionFieldsOffset))
+        file.write(self.toBigI(160))
+        file.write(self.toBigI(self.falloutPlaneOffset))
+        file.write(self.toBigI(self.numberOfGoals))
+        file.write(self.toBigI(self.goalsOffset))
+        file.write(self.toBigI(self.numberOfGoals));
+        self.writeZeroBytes(file, 1)
+        file.write(self.toBigI(self.numberOfBumpers))
+        file.write(self.toBigI(self.bumpersOffset))
+        file.write(self.toBigI(self.numberOfJamabars))
+        file.write(self.toBigI(self.jamabarOffset))
+        file.write(self.toBigI(self.numberOfBananas))
+        file.write(self.toBigI(self.bananasOffset))
+        self.writeZeroBytes(file, 10)
+        self.writeZeroBytes(file, 8)
+        file.write(self.toBigI(self.numberOfLevelModels))
+        file.write(self.toBigI(self.levelModelsOffset))
+        self.writeZeroBytes(file, 8)
+        file.write(self.toBigI(self.numberOfBackgroundModels))
+        file.write(self.toBigI(self.backgroundModelsOffset))
+        self.writeZeroBytes(file, 8)
+        self.writeZeroBytes(file, 8)
+        file.write(self.toBigI(self.numberOfReflectiveObjects))
+        file.write(self.toBigI(self.reflectiveObjectsOffset))
+        self.writeZeroBytes(file, 30)
+        
             
     def writeLZ(self):
         """Save an SMB LZ File"""
-        with open(self.filepath, 'w') as file:
-            self.writeZeroBytes(file, 8)
+        with open(self.filepath, 'wb') as file:
+            self.writeHeader(file)
+            
+    def toBigI(self, number):
+        return struct.pack('>I', number)
             
 
 
