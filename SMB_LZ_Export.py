62bb7d6fc479a1dd1452cec5b10de4d6628751a9
Bobjrsenior <bobjrsenior@gmail.com>
2016-09-13 16:19:27 -0400

    Initial Example Script. Writes 8 0 bytes into a designated file. Also has variables for header with default values

---
+++ SMB_LZ_Export.py
@@ -0,0 +1,101 @@
+bl_info = {
+    "name": "SMB LZ format",
+    "category": "Import-Export",
+    "description": "Exports to the SMB LZ format",
+    "version": (0, 0, 1)}
+
+import os    
+import bpy
+import mathutils
+import bpy_extras.io_utils
+
+from bpy.props import (
+        BoolProperty,
+        FloatProperty,
+        StringProperty,
+        EnumProperty,
+        )
+
+class SMBLZExporter(bpy.types.Operator):
+    """Export to an SMB LZ File"""      # blender will use this as a tooltip for menu items and buttons.
+    bl_idname = "export_smb.lz"        # unique identifier for buttons and menu items to reference.
+    bl_label = "Export SMB LZ"         # display name in the interface.
+    bl_options = {'PRESET'}
+    
+    # the two files we want to select
+    filepath = bpy.props.StringProperty(subtype='FILE_PATH')
+    
+    numCollisionFields = 0;
+    offsetToCollisionFields = 0
+    sizeOfHeader = 160
+    falloutPlaneOffset = 0
+    falloutPlaneY = 0
+    numberOfGoals = 0
+    goalsOffset = 0
+    numberOfBumpers = 0
+    bumpersOffset = 0
+    numberOfJamabars = 0
+    jamabarOffset = 0
+    numberOfBananas = 0
+    bananasOffset = 0
+    numberOfLevelModels = 0
+    levelModelsOffset = 0
+    numberOfBackgroundModels = 0
+    backgroundModelsOffset = 0
+    numberOfReflectiveObjects = 0
+    reflectiveObjectsOffset = 0
+    
+    
+    filename_ext = ".lz"
+    filter_glob = StringProperty(
+            default="*.lz",
+            options={'HIDDEN'},
+            )
+
+
+    def execute(self, context):        # execute() is called by blender when running the operator.
+
+        # The original script
+        scene = context.scene
+        #for obj in scene.objects:
+        #    obj.location.x += 1.0
+        self.writeLZ()
+        return {'FINISHED'}            # this lets blender know the operator finished successfully.
+        
+    def invoke(self, context, event):
+        context.window_manager.fileselect_add(self)
+        return {'RUNNING_MODAL'}
+    
+    def writeZeroBytes(self, file, numZeros):
+        zeroChar = chr(0)
+        for i in range(0, numZeros):
+            file.write(zeroChar)
+            
+    def writeLZ(self):
+        """Save an SMB LZ File"""
+        with open(self.filepath, 'w') as file:
+            self.writeZeroBytes(file, 8)
+            
+
+
+def menu_func_export(self, context):
+    self.layout.operator(SMBLZExporter.bl_idname, text="SMB LZ (.lz)")
+
+
+def register():
+    bpy.utils.register_class(SMBLZExporter)
+    bpy.types.INFO_MT_file_export.append(menu_func_export)
+    
+
+
+def unregister():
+    bpy.utils.unregister_class(SMBLZExporter)
+    bpy.types.INFO_MT_file_export.remove(menu_func_export)
+
+
+# This allows you to run the script directly from blenders text editor
+# to test the addon without having to install it.
+if __name__ == "__main__":
+    register()
+    
+    
